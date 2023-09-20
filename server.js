61313aa425c4fbc72018cb43df405175e5ef1781
Bryan Arambula <arambula722@gmail.com>
2023-09-20 17:52:02 -0400

    added the build folder form react

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-// server.js
 import express from "express";
 import bodyParser from "body-parser";
 import qr from "qr-image";
 import cors from "cors";
+import path from "path"; // <-- Import path module
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
+// Serve the built React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.post("/generate-qr", (req, res) => {
   const { text } = req.body;
   const qr_png = qr.image(text, { type: "png" });
   res.setHeader("Content-Type", "image/png");
   qr_png.pipe(res);
+});
+
+// Catch-all route for handling client-side routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 app.listen(process.env.PORT || 4000, () => {
